99afe62aa568eee91b54329819d75a26756d27e5
juan saavedra <juans0520@gmail.com>
2026-08-27 12:57:08 -0500

    chore: borrar entidades Module/Submodule/UserSubmodule sin uso
    Duplicaban AspNetModule/AspNetSubModule/AspNetUserSubModule (que si se
    usan). Sin DbSet registrado, sin ninguna referencia real en el codigo.

--- Backend/Domain/Entities/Identity/Module.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Domain.Entities.Identity;
-
-public class Module : EntityBase
-{
-    public string Identificador { get; set; }
-    public string Nombre { get; set; }
-    public List<Submodule> Submodules { get; set; } = new List<Submodule>();
-}
